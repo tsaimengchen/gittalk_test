7b4b8dbc648c5355c329ca2564d29871c49f78e5
蔡孟辰 <b06502020@ntu.edu.tw>
2021-07-06 17:43:32 +0800

    test print

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-console.log("testsets")
+console.log("testsets");
 const gitalk = new Gitalk({
   clientID: '91beda1e8145fbaebf77',
   clientSecret: '6e4579604cb57847d0f11ed37d5260598d138568',
@@ -7,6 +7,6 @@ const gitalk = new Gitalk({
   admin: ['littlecatt'],
   id: location.href,      // Ensure uniqueness and length less than 50
   distractionFreeMode: false  // Facebook-like distraction free mode
-})
+});
 
-gitalk.render('gitalk-container')
+gitalk.render('gitalk-container');
