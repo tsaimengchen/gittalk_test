7a6c2b411ea1ddfcbf46b85f9c3cf1dad4546975
蔡孟辰 <b06502020@ntu.edu.tw>
2021-07-06 17:42:42 +0800

    test print

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
+console.log("testsets")
 const gitalk = new Gitalk({
   clientID: '91beda1e8145fbaebf77',
   clientSecret: '6e4579604cb57847d0f11ed37d5260598d138568',
   repo: 'gittalk_test',      // The repository of store comments,
   owner: 'littlecatt',
   admin: ['littlecatt'],
-  id: location.pathname,      // Ensure uniqueness and length less than 50
+  id: location.href,      // Ensure uniqueness and length less than 50
   distractionFreeMode: false  // Facebook-like distraction free mode
 })
 
